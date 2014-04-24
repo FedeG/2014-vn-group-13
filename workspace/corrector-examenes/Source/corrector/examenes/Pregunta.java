7b2be5febb3cdf6ef889c39c46f86054a305da6a
Federico Gonzalez <federicog@exo.com.ar>
2014-04-24 10:27:03 -0300

    La pregunta verifica si las respuesta es correcta y compara las respuestas sin tener en cuenta las mayusculas y minisculas (ejemplo: 'respuesta correcta' es lo mismo que 'Respuesta Correcta' y que 'RESPUESTA CORRECTA' )

--- workspace/corrector-examenes/Source/corrector/examenes/Pregunta.java
+++ workspace/corrector-examenes/Source/corrector/examenes/Pregunta.java
@@ -4,5 +4,9 @@ public class Pregunta {
 	public int pesoEspecifico;
 	public String respuestaCorrecta;
 	public String descripcion;
+	
+	public boolean esRespuestaCorrecta(String respuestaAlumno){
+		return pregunta.respuestaCorrecta.equalsIgnoreCase(respuestaAlumno);
+	}
 }
 
