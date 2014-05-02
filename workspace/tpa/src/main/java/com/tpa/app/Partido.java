71e4721e8b84a779d4cf22369e2d0d4789dec6c2
fedeG <federicogonzalez761@gmail.com>
2014-05-02 13:34:43 -0300

    Partido con SortedMap para el manejo del diccionario de Collection

--- workspace/tpa/src/main/java/com/tpa/app/Partido.java
+++ workspace/tpa/src/main/java/com/tpa/app/Partido.java
@@ -1,21 +1,28 @@
 package com.tpa.app;
 
 import java.time.LocalDateTime;
-import java.util.Collection; //Buscar de SortedSet o SortedMap
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class Partido {
 
 	private String lugar;
 	private LocalDateTime fechaHora;
 	private int cupo;
-	private Collection<Jugador> jugadoresEstandar;
-	private Collection<Jugador> jugadoresSolidarios;
-	private Collection<Jugador> jugadoresCondicionales;
+	private SortedMap<Integer, Collection<Jugador>> jugadoresFijos;
+	private SortedMap<Integer, Collection<Jugador>> jugadoresCondicionales;
 
 	public Partido(LocalDateTime fecha_y_hora, String lugar, int cupo) {
 		this.setFechaHora(fecha_y_hora);
 		this.setLugar(lugar);
 		this.setCupo(cupo);
+		this.jugadoresCondicionales = new TreeMap<Integer, Collection<Jugador>>();
+		this.jugadoresCondicionales.put(0, new ArrayList<Jugador>()); // Jugadores condicionales
+		this.jugadoresFijos = new TreeMap<Integer, Collection<Jugador>>();
+		this.jugadoresFijos.put(0, new ArrayList<Jugador>()); // Jugadores estandar
+		this.jugadoresFijos.put(1, new ArrayList<Jugador>()); // Jugadores solidarios
 	}
 
 	public String getLugar() {
@@ -41,29 +48,37 @@ public class Partido {
 	public void setFechaHora(LocalDateTime fechaHora) {
 		this.fechaHora = fechaHora;
 	}
-
-	public Collection<Jugador> getJugadoresEstandar() {
-		return jugadoresEstandar;
+	
+	public SortedMap<Integer, Collection<Jugador>> getJugadoresFijos() {
+		return jugadoresFijos;
 	}
 
-	public Collection<Jugador> getJugadoresSolidarios() {
-		return jugadoresSolidarios;
-	}
-
-	public Collection<Jugador> getJugadoresCondicionales() {
+	public SortedMap<Integer, Collection<Jugador>> getJugadoresCondicionales() {
 		return jugadoresCondicionales;
 	}
-
+	
 	public void inscribirCondicional(Jugador jugador) {
-		this.jugadoresCondicionales.add(jugador);
+		this.getJugadoresCondicionales().get(0).add(jugador);
 	}
 
 	public void inscribirEstadar(Jugador jugador) {
-		this.jugadoresEstandar.add(jugador);
+		this.getJugadoresFijos().get(0).add(jugador);
 	}
 
 	public void inscribirSolidario(Jugador jugador) {
-		this.jugadoresSolidarios.add(jugador);
+		this.getJugadoresFijos().get(1).add(jugador);
+	}
+
+	public Collection<Jugador> getJugadoresSolidarios() {
+		return this.getJugadoresFijos().get(1);
+	}
+	
+	public Collection<Jugador> getJugadoresEstandar() {
+		return this.getJugadoresFijos().get(0);
+	}
+	
+	public Collection<Jugador> getJugadoresConCondicion() {
+		return this.getJugadoresCondicionales().get(0);
 	}
 
 }
