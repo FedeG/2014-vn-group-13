78b529a490c6b0078eb2c2547f30862f09e0a3e7
Pablo Pereyra Molina <pablo_3112@hotmail.com>
2014-05-13 01:00:31 -0300

    Se agrega metodo para formar equipos tentativos.

--- workspace/tpa/src/main/java/com/tpa/app/Administrador.java
+++ workspace/tpa/src/main/java/com/tpa/app/Administrador.java
@@ -2,6 +2,7 @@ package com.tpa.app;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.stream.Collectors;
 import java.time.LocalDateTime;
 
 public class Administrador {
@@ -9,15 +10,18 @@ public class Administrador {
 	
 	public Administrador() {
 		this.partidos = new ArrayList<Partido>();
-	}
-	
+	}	
 	public Collection<Partido> getPartidos() {
 		return this.partidos;
 	}
-
-	public void crearPartido(LocalDateTime fecha_y_hora, String lugar, int cupo) {
+	public Partido crearPartido(LocalDateTime fecha_y_hora, String lugar, int cupo) {
 		Partido partidoNuevo = new Partido(fecha_y_hora, lugar, cupo);
 		this.partidos.add(partidoNuevo);
+		return partidoNuevo;
 	}
-
+	public void generarEquiposTentativos(Partido partido)
+	{
+		partido.setEquipoA(partido.getInscripciones().stream().limit(5).collect(Collectors.toCollection(()-> new ArrayList<Inscripcion>())));
+		partido.setEquipoB(partido.getInscripciones().stream().skip(5).collect(Collectors.toCollection(()-> new ArrayList<Inscripcion>())));
+	}
 }
