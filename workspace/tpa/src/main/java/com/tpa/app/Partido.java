16ed2e8642831a0fe35a3eb199e5e75879724018
Pablo Pereyra Molina <pablo_3112@hotmail.com>
2014-05-13 01:06:19 -0300

    Se reemplazan los diccionarios por una lista ordenada de Inscripciones.Se agregan listas con equipos.Se modifican los metodos de inscripcion.Se pueden dar de baja jugadores.falta desarrollar notificarAdministrador

--- workspace/tpa/src/main/java/com/tpa/app/Partido.java
+++ workspace/tpa/src/main/java/com/tpa/app/Partido.java
@@ -1,84 +1,107 @@
 package com.tpa.app;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import com.tpa.app.Inscripcion.PrioridadesInscripciones;
 
 public class Partido {
 
-	private String lugar;
-	private LocalDateTime fechaHora;
 	private int cupo;
-	private SortedMap<Integer, Collection<Jugador>> jugadoresFijos;
-	private SortedMap<Integer, Collection<Jugador>> jugadoresCondicionales;
+	private LocalDateTime fechaHora;
+	private String lugar;
+	private PriorityQueue<Inscripcion> inscripciones;
+	private List<Inscripcion> equipoA;
+	private List<Inscripcion> equipoB;
 
 	public Partido(LocalDateTime fecha_y_hora, String lugar, int cupo) {
 		this.setFechaHora(fecha_y_hora);
 		this.setLugar(lugar);
 		this.setCupo(cupo);
-		this.jugadoresCondicionales = new TreeMap<Integer, Collection<Jugador>>();
-		this.jugadoresCondicionales.put(0, new ArrayList<Jugador>()); // Jugadores condicionales
-		this.jugadoresFijos = new TreeMap<Integer, Collection<Jugador>>();
-		this.jugadoresFijos.put(0, new ArrayList<Jugador>()); // Jugadores estandar
-		this.jugadoresFijos.put(1, new ArrayList<Jugador>()); // Jugadores solidarios
+		this.inscripciones =  new PriorityQueue<Inscripcion>((x, y) -> x.dameTuPrioridad().compareTo(y.dameTuPrioridad()));
 	}
 
 	public String getLugar() {
 		return lugar;
 	}
-
 	public void setLugar(String lugar) {
 		this.lugar = lugar;
 	}
-
+	public List<Inscripcion> getEquipoB() {
+		return equipoB;
+	}
+	public void setEquipoB(List<Inscripcion> equipoB) {
+		this.equipoB = equipoB;
+	}
+	public List<Inscripcion> getEquipoA() {
+		return equipoA;
+	}
+	public void setEquipoA(List<Inscripcion> equipoA) {
+		this.equipoA = equipoA;
+	}
 	public int getCupo() {
 		return cupo;
 	}
-
 	public void setCupo(int cupo) {
 		this.cupo = cupo;
 	}
-
 	public LocalDateTime getFechaHora() {
 		return fechaHora;
 	}
-
 	public void setFechaHora(LocalDateTime fechaHora) {
 		this.fechaHora = fechaHora;
 	}
+	public PriorityQueue<Inscripcion> getInscripciones() {
+		return this.inscripciones;
+	}
+		
+	public void inscribir(Inscripcion inscripcion) {
+		this.getInscripciones().add(inscripcion);
+		inscripcion.jugador.avisarAmigos(this);
+		if(verificarCupoCompleto())
+			notificarAdministrador("Ya hay 10 jugadores inscriptos que pueden jugar.");
+	}
+
+	public Inscripcion obtenerInscripcionDe(Jugador jugador)
+	{
+		Inscripcion inscripcion = null;
+		for (Inscripcion inscr : getInscripciones())  
+		{
+			if( inscr.jugador.equals(jugador))
+			{
+				inscripcion = inscr;
+				break;
+			}
+		}
+		return inscripcion;
+	}
+	public void darDeBaja(Jugador jugador, String motivo)
+	{
+		Inscripcion inscripcion = obtenerInscripcionDe(jugador);
+		if (inscripcion == null)
+			return;
+		inscripcion.setActivo(false);
+		jugador.agregarInfraccion(new Infraccion(motivo, LocalDateTime.now()));
+		if(!verificarCupoCompleto())
+			notificarAdministrador("Se dio de baja un jugador.");
+	}
+	public void darDeBaja(Jugador jugador, Jugador jugadorReemplaza)
+	{
+		Inscripcion inscripcion = obtenerInscripcionDe(jugador);
+		if (inscripcion == null)
+			return;
+		inscripcion.setActivo(false);
+		this.inscribir(new InscripcionEstandar(jugadorReemplaza));
+	}
+	public boolean verificarCupoCompleto()
+	{
+		int cantInscripcionesEstandar = (int) getInscripciones().stream().filter(i ->i.getActivo() && i.dameTuPrioridad() == PrioridadesInscripciones.Estandar).count();
+		return cantInscripcionesEstandar > 10;
+	}
 	
-	public SortedMap<Integer, Collection<Jugador>> getJugadoresFijos() {
-		return jugadoresFijos;
+	public void notificarAdministrador(String mensaje)
+	{
+		//TODO: aca debo enviar un mail al administrador 
 	}
-
-	public SortedMap<Integer, Collection<Jugador>> getJugadoresCondicionales() {
-		return jugadoresCondicionales;
-	}
-	
-	public void inscribirCondicional(Jugador jugador) {
-		this.getJugadoresCondicionales().get(0).add(jugador);
-	}
-
-	public void inscribirEstadar(Jugador jugador) {
-		this.getJugadoresFijos().get(0).add(jugador);
-	}
-
-	public void inscribirSolidario(Jugador jugador) {
-		this.getJugadoresFijos().get(1).add(jugador);
-	}
-
-	public Collection<Jugador> getJugadoresSolidarios() {
-		return this.getJugadoresFijos().get(1);
-	}
-	
-	public Collection<Jugador> getJugadoresEstandar() {
-		return this.getJugadoresFijos().get(0);
-	}
-	
-	public Collection<Jugador> getJugadoresConCondicion() {
-		return this.getJugadoresCondicionales().get(0);
-	}
-
 }
