f7d12a4285adc6831b82417511922893db18cc78
fedeG <federicogonzalez761@gmail.com>
2014-06-11 01:43:44 -0300

    Generador de Equipos

---
+++ workspace/tpa/src/main/java/com/tpa/app/GeneradorDeEquipos.java
@@ -0,0 +1,14 @@
+package com.tpa.app;
+
+import java.util.ArrayList;
+
+public class GeneradorDeEquipos {
+	
+	public void ordenarJugadores(ArrayList<Criterio> criterios, Partido partido){
+		
+	}
+	
+	public void dividirEquipos(Divisor divisor, Partido partido){
+		
+	}
+}
