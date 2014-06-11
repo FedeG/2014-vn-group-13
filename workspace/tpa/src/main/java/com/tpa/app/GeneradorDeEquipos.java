f4f474c4bcbbd9a8864c3ed80fb85a210d84d62a
fedeG <federicogonzalez761@gmail.com>
2014-06-11 02:32:14 -0300

    Generador de Equipos con division de equipos

--- workspace/tpa/src/main/java/com/tpa/app/GeneradorDeEquipos.java
+++ workspace/tpa/src/main/java/com/tpa/app/GeneradorDeEquipos.java
@@ -3,12 +3,16 @@ package com.tpa.app;
 import java.util.ArrayList;
 
 public class GeneradorDeEquipos {
-	
+
 	public void ordenarJugadores(ArrayList<Criterio> criterios, Partido partido){
 		
 	}
-	
+
 	public void dividirEquipos(Divisor divisor, Partido partido){
-		
+		ArrayList<Inscripcion> equipoA, equipoB;
+		equipoA = divisor.generarEquipoA(partido.getInscripciones());
+		equipoB = divisor.generarEquipoB(partido.getInscripciones());
+		partido.equiposAJugar(equipoA, equipoB);
 	}
+
 }
