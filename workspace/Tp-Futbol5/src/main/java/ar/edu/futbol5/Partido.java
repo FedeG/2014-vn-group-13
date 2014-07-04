684cb85210c0e4ffca9ec2e1dba3f71c748697cd
Pablo Pereyra Molina <pablo_3112@hotmail.com>
2014-07-04 09:53:10 -0300

    Se utiliza clase CriterioDistribucion para la distribucion del os equipos y asi evitar switch statements con el int que existia

--- workspace/Tp-Futbol5/src/main/java/ar/edu/futbol5/Partido.java
+++ workspace/Tp-Futbol5/src/main/java/ar/edu/futbol5/Partido.java
@@ -1,5 +1,7 @@
 package ar.edu.futbol5;
 
+import ar.edu.futbol5.distribucion.CriterioDistribucion;
+import ar.edu.futbol5.distribucion.DistribucionParImpar;
 import ar.edu.futbol5.excepciones.BusinessException;
 import ar.edu.futbol5.ordenamiento.CriterioOrdenamiento;
 import ar.edu.futbol5.ordenamiento.OrdenamientoPorHandicap;
@@ -15,8 +17,7 @@ public class Partido {
 	private Equipo equipo2;
 	private EstadoPartido estado;
 	private CriterioOrdenamiento criterioOrdenamiento;
-	private int distribucionEquipos; // 5 es par/impar, 16 = 1,4,5,8,9 vs.
-										// 2,3,6,7,10
+	private CriterioDistribucion distribucionEquipos;
 
 	public enum EstadoPartido {
 		Abierto, EquiposGenerados, Cerrado
@@ -25,7 +26,7 @@ public class Partido {
 	public Partido() {
 		inscriptos = new ArrayList<Jugador>();
 		estado = EstadoPartido.Abierto;
-		distribucionEquipos = 5; // par/impar
+		distribucionEquipos = new DistribucionParImpar();
 		criterioOrdenamiento = new OrdenamientoPorHandicap();
 	}
 
@@ -35,36 +36,9 @@ public class Partido {
 
 		equipo1 = new Equipo();
 		equipo2 = new Equipo();
-		List<Integer> posiciones1 = new ArrayList<Integer>();
-		List<Integer> posiciones2 = new ArrayList<Integer>();
-
-		if (distribucionEquipos == 5) {
-			posiciones1.add(0);
-			posiciones1.add(2);
-			posiciones1.add(4);
-			posiciones1.add(6);
-			posiciones1.add(8);
-			posiciones2.add(1);
-			posiciones2.add(3);
-			posiciones2.add(5);
-			posiciones2.add(7);
-			posiciones2.add(9);
-		} else {
-			// distribucionEquipos == 16 que ordena de esta manera
-			posiciones1.add(0);
-			posiciones1.add(3);
-			posiciones1.add(4);
-			posiciones1.add(7);
-			posiciones1.add(8);
-			posiciones2.add(1);
-			posiciones2.add(2);
-			posiciones2.add(5);
-			posiciones2.add(6);
-			posiciones2.add(9);
-		}
-
-		equipo1.distribuir(this.ordenarEquipos(), posiciones1);
-		equipo2.distribuir(this.ordenarEquipos(), posiciones2);
+		
+		this.ordenarEquipos();
+		this.distribuirEquipos();
 
 		estado = EstadoPartido.EquiposGenerados;
 	}
@@ -80,6 +54,10 @@ public class Partido {
 
 	public List<Jugador> ordenarEquipos() {
 		return criterioOrdenamiento.ordenar(this);
+	}
+	public void distribuirEquipos(){
+		this.equipo1.setJugadores(distribucionEquipos.ObtenerEquipo1(this));
+		this.equipo2.setJugadores(distribucionEquipos.ObtenerEquipo2(this));
 	}
 
 	void inscribir(Jugador jugador) {
@@ -118,7 +96,7 @@ public class Partido {
 		this.criterioOrdenamiento = criterioOrdenamiento;
 	}
 
-	public void setDistribucionEquipos(int distribucionEquipos) {
+	public void setDistribucionEquipos(CriterioDistribucion distribucionEquipos) {
 		this.distribucionEquipos = distribucionEquipos;
 	}
 
