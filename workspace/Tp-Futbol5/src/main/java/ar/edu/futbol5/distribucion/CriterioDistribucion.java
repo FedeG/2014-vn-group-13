cecbb43d94cf6e24ec3910fae8e67a5398291a95
root <root@debian>
2014-07-04 17:56:37 -0300

    Method chaining y code smell que no me acuerdo el nombre (ordenamiento)
    Pasaba datos de mas que no se usaban y ademas tenia un Method chaining.

--- workspace/Tp-Futbol5/src/main/java/ar/edu/futbol5/distribucion/CriterioDistribucion.java
+++ workspace/Tp-Futbol5/src/main/java/ar/edu/futbol5/distribucion/CriterioDistribucion.java
@@ -4,21 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import ar.edu.futbol5.Jugador;
-import ar.edu.futbol5.Partido;
 
 public class CriterioDistribucion {
-	List<Integer> posicionesEquipo1 = new ArrayList<Integer>();
-	List<Integer> posicionesEquipo2 = new ArrayList<Integer>();
-	
-	public List<Jugador> ObtenerEquipo1(Partido partido) {
-		return ObtenerEquipo(partido, posicionesEquipo1);
-	}
-	public List<Jugador> ObtenerEquipo2(Partido partido) {
-		return ObtenerEquipo(partido, posicionesEquipo2);
-	}
-	private List<Jugador> ObtenerEquipo(Partido partido, List<Integer> posiciones) {
+	public List<Integer> posicionesEquipo1 = new ArrayList<Integer>();
+	public List<Integer> posicionesEquipo2 = new ArrayList<Integer>();
+
+	public List<Jugador> ObtenerEquipo(List<Jugador> inscripciones, List<Integer> posiciones) {
 		ArrayList<Jugador> jugadores = new ArrayList<Jugador>();	
-		posiciones.forEach(p -> jugadores.add(partido.getInscriptos().get(p)));	
+		posiciones.forEach(p -> jugadores.add(inscripciones.get(p)));	
 		return jugadores;
 	}
 }
