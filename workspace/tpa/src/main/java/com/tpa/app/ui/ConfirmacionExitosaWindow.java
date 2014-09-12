c9a936681cafab3db41e0ab2d67cdcf4f3bc7432
fedeG <federicogonzalez761@gmail.com>
2014-09-12 01:05:02 -0300

    Ventana confirmacion

--- workspace/tpa/src/main/java/com/tpa/app/ui/ConfirmacionExitosaWindow.java
+++ workspace/tpa/src/main/java/com/tpa/app/ui/ConfirmacionExitosaWindow.java
@@ -1,43 +1,30 @@
 package com.tpa.app.ui;
 
-import org.uqbar.arena.actions.MessageSend;
-import org.uqbar.arena.widgets.Button;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.windows.SimpleWindow;
 import org.uqbar.arena.windows.WindowOwner;
 
 import com.tpa.app.domain.PantallaPrincipal;
 
-public class ConfirmacionExitosaWindow extends  SimpleWindow<PantallaPrincipal> {
+public class ConfirmacionExitosaWindow extends SimpleWindow<PantallaPrincipal> {
 
 	public ConfirmacionExitosaWindow(WindowOwner parent) {
 		super(parent, new PantallaPrincipal());
-
 	}
 
-	
 	@Override
 	protected void createMainTemplate(Panel mainPanel) {
-		
-		super.createMainTemplate(mainPanel);
 		this.setTitle("");
-		this.setTaskDescription("Confirmación Exitosa :D :D :D");
-		
-
+		this.setTaskDescription("\n   Confirmación Exitosa !!!   \n");
+		super.createMainTemplate(mainPanel);
 	}
-
-
 
 	@Override
 	protected void addActions(Panel arg0) {
-		// TODO Auto-generated method stub
-		
 	}
-
 
 	@Override
 	protected void createFormPanel(Panel arg0) {
-		// TODO Auto-generated method stub
-		
 	}
+
 }
