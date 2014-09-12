22b791702a65a8d29ed430eed4d987baa887d7a2
EzequielAyzenberg <ezequiel_ay@hotmail.com>
2014-09-12 10:44:48 -0300

    Estetica de controles :3

--- workspace/tpa/src/main/java/com/tpa/app/ui/PantallaPrincipalWindow.java
+++ workspace/tpa/src/main/java/com/tpa/app/ui/PantallaPrincipalWindow.java
@@ -1,10 +1,13 @@
 package com.tpa.app.ui;
 
 import org.uqbar.arena.actions.MessageSend;
+import org.uqbar.arena.layout.VerticalLayout;
 import org.uqbar.arena.widgets.Button;
+import org.uqbar.arena.widgets.Label;
 import org.uqbar.arena.widgets.Panel;
 import org.uqbar.arena.windows.SimpleWindow;
 import org.uqbar.arena.windows.WindowOwner;
+
 import com.tpa.app.domain.PantallaPrincipal;
 
 
@@ -16,24 +19,28 @@ public class PantallaPrincipalWindow extends SimpleWindow<PantallaPrincipal> {
 	
 	@Override
 	protected void createMainTemplate(Panel mainPanel) {
-		this.setTitle("Operaciones");
-		this.setTaskDescription("Elija una operación a realizar");
+		this.setTitle("Organizador de Partidos de Futbol - DDS 2014");
+		this.setTaskDescription("Elija una operacion a realizar");
 		super.createMainTemplate(mainPanel);
 	}
 
 	@Override
-	protected void createFormPanel(Panel mainPanel) {}
+	protected void createFormPanel(Panel mainPanel){
+		mainPanel.setLayout(new VerticalLayout());
+		new Label(mainPanel).setText("- Operaciones -").setWidth(430);
+		new Button(mainPanel)
+			.setCaption("Generar Equipos")
+			.onClick(new MessageSend(this, "generarEquipos"));
+	
+		new Button(mainPanel)
+			.setCaption("Buscar Jugadores")
+			.onClick(new MessageSend(this, "buscarJugadores"));
+	}
 	
 	@Override
 	protected void addActions(Panel actionsPanel) {
 		
-		new Button(actionsPanel)
-			.setCaption("Generar Equipos")
-			.onClick(new MessageSend(this, "generarEquipos"));
-	
-		new Button(actionsPanel)
-			.setCaption("Buscar Jugadores")
-			.onClick(new MessageSend(this, "buscarJugadores"));
+		
 	}
 	
 	public void generarEquipos() {
