9f3a8a59dc4056074275b70f8984700aa54a61ac
fedeG <federicogonzalez761@gmail.com>
2014-09-12 04:26:50 -0300

    Infraccion nueva con cambios minimos y mustra de date and time copado

--- workspace/tpa/src/main/java/com/tpa/app/Infraccion.java
+++ workspace/tpa/src/main/java/com/tpa/app/Infraccion.java
@@ -1,14 +1,34 @@
 package com.tpa.app;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Infraccion {
-	String motivo;
-	LocalDateTime momento;
+	
+	private String motivo;
+	private LocalDateTime momento;
 	
 	public Infraccion(String motivo, LocalDateTime momento)
 	{
 		this.momento = momento;
 		this.motivo = motivo;
+	}
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public LocalDateTime getMomento() {
+		return momento;
+	}
+	
+	public String getDate(){
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		return momento.format(formatter);
+	}
+
+	public String getTime(){
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+		return momento.format(formatter);
 	}
 }
