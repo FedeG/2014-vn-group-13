5861d9453669a976b0b4754275a6f58b9c907242
fedeG <federicogonzalez761@gmail.com>
2014-09-15 01:45:34 -0300

    Vista con todo el uso de campos dinamicos y todos los datos desde el administrador (criterios, partidos y divisores)

--- workspace/tpa/src/main/java/com/tpa/app/ui/GenerarEquiposWindow.java
+++ workspace/tpa/src/main/java/com/tpa/app/ui/GenerarEquiposWindow.java
@@ -18,19 +18,16 @@ import org.uqbar.arena.widgets.RadioSelector;
 
 import com.tpa.app.ByIndex;
 import com.tpa.app.Criterio;
+import com.tpa.app.Divisor;
 import com.tpa.app.GeneradorDeEquipos;
 import com.tpa.app.Inscripcion;
 import com.tpa.app.Partido;
-import com.tpa.app.PartidoYaConfirmadoExcepcion;
-import com.tpa.app.PorHandicap;
-import com.tpa.app.PorPromedio;
 import com.tpa.app.domain.BuscadorPartidos;
 
 public class GenerarEquiposWindow extends SimpleWindow<BuscadorPartidos> {
 
 	public GenerarEquiposWindow(WindowOwner parent) {
 		super(parent, new BuscadorPartidos());
-		this.getModelObject().search();
 	}
 
 	@Override
@@ -57,10 +54,7 @@ public class GenerarEquiposWindow extends SimpleWindow<BuscadorPartidos> {
 		volver.setWidth(200);
 		volver.onClick(new MessageSend(this, "close"));
 
-		// Deshabilitar los botones si no hay ningún elemento seleccionado en
-		// la grilla.
-		NotNullObservable elementSelected = new NotNullObservable(
-				"partidoSeleccionado");
+		NotNullObservable elementSelected = new NotNullObservable("partidoSeleccionado");
 		generar.bindEnabled(elementSelected);
 	}
 
@@ -72,11 +66,9 @@ public class GenerarEquiposWindow extends SimpleWindow<BuscadorPartidos> {
 		searchOrdenamientoPanel.setLayout(new VerticalLayout());
 
 		new Label(searchOrdenamientoPanel).setText("Criterios de Ordenamiento");
-		RadioSelector<String> radioSelectorOrdenamiento = new RadioSelector<>(
-				searchOrdenamientoPanel);
+		RadioSelector<String> radioSelectorOrdenamiento = new RadioSelector<>(searchOrdenamientoPanel);
 		radioSelectorOrdenamiento.setWidth(20);
-		radioSelectorOrdenamiento
-				.bindValueToProperty("ordenamientoSeleccionado");
+		radioSelectorOrdenamiento.bindValueToProperty("ordenamientoSeleccionado");
 		radioSelectorOrdenamiento.bindItemsToProperty("ordenamientos");
 		this.getModelObject().setOrdenamientoSeleccionado("Por Promedio");
 
@@ -84,12 +76,11 @@ public class GenerarEquiposWindow extends SimpleWindow<BuscadorPartidos> {
 		searchSeleccionPanel.setLayout(new VerticalLayout());
 
 		new Label(searchSeleccionPanel).setText("Criterios de Seleccion");
-		RadioSelector<String> radioSelectorSeleccion = new RadioSelector<>(
-				searchSeleccionPanel);
+		RadioSelector<String> radioSelectorSeleccion = new RadioSelector<>(searchSeleccionPanel);
 		radioSelectorSeleccion.setWidth(20);
 		radioSelectorSeleccion.bindValueToProperty("seleccionSeleccionada");
 		radioSelectorSeleccion.bindItemsToProperty("selecciones");
-		this.getModelObject().setSeleccionSeleccionada("Par/Impar");
+		this.getModelObject().setSeleccionSeleccionada("ParesImpares");
 	}
 
 	protected void crearGrillaPartidos(Panel mainPanel) {
@@ -97,91 +88,57 @@ public class GenerarEquiposWindow extends SimpleWindow<BuscadorPartidos> {
 		table.setHeigth(100);
 		table.setWidth(380);
 
-		table.bindItemsToProperty("resultados");
+		table.bindItemsToProperty("administrador.partidos");
 		table.bindValueToProperty("partidoSeleccionado");
 
 		/* Grid Description */
 
-		new Column<Partido>(table).setTitle("Fecha y Hora").setFixedSize(200)
-				.bindContentsToTransformer(new FechaTransfomer());
+		new Column<Partido>(table)
+			.setTitle("Fecha y Hora")
+			.setFixedSize(200)
+			.bindContentsToTransformer(new FechaTransfomer());
 
-		new Column<Partido>(table).setTitle("Lugar").setFixedSize(180)
-				.bindContentsToProperty("lugar");
+		new Column<Partido>(table)
+			.setTitle("Lugar")
+			.setFixedSize(180)
+			.bindContentsToProperty("lugar");
 	}
 
 	public void generar() {
-		if(getModelObject().getPartidoSeleccionado().getConfirmado())
-			throw new PartidoYaConfirmadoExcepcion();
+		this.getModelObject().getPartidoSeleccionado().verificarConfirmacion();
 		GeneradorDeEquipos generador = new GeneradorDeEquipos();
-
 		List<Criterio> critOrden = new ArrayList<Criterio>();
 		critOrden = crearListaSegunRadioButton();
-
-		List<Inscripcion> inscripciones = generador.ordenarJugadores(critOrden,
-				this.getModelObject().getPartidoSeleccionado());
-
+		List<Inscripcion> inscripciones = generador.ordenarJugadores(critOrden, this.getModelObject().getPartidoSeleccionado());
 		ByIndex divisor = crearDivisorSegunRadioButton();
-		generador.dividirEquipos(divisor, this.getModelObject()
-				.getPartidoSeleccionado(), inscripciones);
-
-		new EquiposGeneradosWindow(this, this.getModelObject()
-				.getPartidoSeleccionado()).open();
+		generador.dividirEquipos(divisor, this.getModelObject().getPartidoSeleccionado(), inscripciones);
+		new EquiposGeneradosWindow(this, this.getModelObject().getPartidoSeleccionado()).open();
 	}
 
 	private ByIndex crearDivisorSegunRadioButton() {
-
-		ArrayList<Integer> indicesEquipoA = new ArrayList<Integer>();
-		ArrayList<Integer> indicesEquipoB = new ArrayList<Integer>();
-
-		if (this.getModelObject().getSeleccionSeleccionada() == "Par/Impar") {
-			indicesEquipoA.add(0);
-			indicesEquipoB.add(1);
-			indicesEquipoA.add(2);
-			indicesEquipoB.add(3);
-			indicesEquipoA.add(4);
-			indicesEquipoB.add(5);
-			indicesEquipoA.add(6);
-			indicesEquipoB.add(7);
-			indicesEquipoA.add(8);
-			indicesEquipoB.add(9);
+		for (Divisor divisor: this.getModelObject().getAdministrador().getDivisores()){
+			if (this.getModelObject().getSeleccionSeleccionada() == ((ByIndex) divisor).getNombre())
+				return (ByIndex) divisor;
 		}
-
-		if (this.getModelObject().getSeleccionSeleccionada() == "1,4,5,8,9") {
-			indicesEquipoA.add(1);
-			indicesEquipoB.add(0);
-			indicesEquipoA.add(4);
-			indicesEquipoB.add(2);
-			indicesEquipoA.add(5);
-			indicesEquipoB.add(3);
-			indicesEquipoA.add(8);
-			indicesEquipoB.add(6);
-			indicesEquipoA.add(9);
-			indicesEquipoB.add(7);
-		}
-		return new ByIndex(indicesEquipoA, indicesEquipoB);
+		return null;
 	}
 
 	private List<Criterio> crearListaSegunRadioButton() {
-		List<Criterio> critOrden = new ArrayList<Criterio>();
-		if (this.getModelObject().getOrdenamientoSeleccionado() == "Por Handicap") {
-			critOrden.add(new PorHandicap());
+		// TODO 
+			// Es una lista para poder usar varios criterios :D
+				// pero hay que cambiar el como se usa porque no se que componente de arena tengo que usar :P
+		List<Criterio> criterios = new ArrayList<Criterio>();
+		for (Criterio criterio: this.getModelObject().getAdministrador().getCriterios()){
+			if (this.getModelObject().getOrdenamientoSeleccionado() == criterio.getNombre())
+				criterios.add(criterio);
 		}
-		if (this.getModelObject().getOrdenamientoSeleccionado() == "Por Promedio") {
-			critOrden.add(new PorPromedio());
-		}
-		if (this.getModelObject().getOrdenamientoSeleccionado() == "Mixto") {
-			critOrden.add(new PorHandicap());
-			critOrden.add(new PorPromedio());
-		}
-		return critOrden;
+		return criterios;
 	}
 
 	@Override
-	protected void createFormPanel(Panel mainPanel) {
-	}
+	protected void createFormPanel(Panel mainPanel) {}
 
 	@Override
-	protected void addActions(Panel mainPanel) {
-	}
+	protected void addActions(Panel mainPanel) {}
 
 }
